8e188e72bb7633681be0bfb28b140d7b36f1d894
Luis Linares <117125597+Linnovel@users.noreply.github.com>
2026-05-23 19:00:26 -0400

    repeat only

--- src/challenges/challengeOne.js
+++ src/challenges/challengeOne.js
@@ -1,1 +1,132 @@
-//Button Challenge
+import '../challengeOne.css'
+import gsap from 'gsap'
+import { GSDevTools } from "gsap/GSDevTools";
+import { TextPlugin } from "gsap/TextPlugin";
+
+// gsap.registerPlugin(GSDevTools);
+
+
+//Repaso Rapido
+//gsap.to la animación va a empezar donde el elemento se encuentre, con el css, y html. Si deseas que se mueva de un lado como tal, tienes que ponerle el css. el tween va a empezar desde donde esta el elemento en su estado original y animarlo, a donde . se le define en el tween gsap.to
+// gsap.to
+    // x: 300, // Mueve el elemento a la coordenada absoluta 300px.
+    // x:  300,
+       // x: "- =300", // Toma la posición donde esté el elemento ahora mismo y le suma 300px.
+
+
+
+//from() es lo contrario a tween to, que hace la animacion desde un estado inicial definido. from define desde su estado inicial que es 180px a su posición natural que es x:0
+//Ve desde la esquina inferior derecha a tu posición natural, que es x:0, y lo hace en 2 segundos
+// gsap.from('.box', {
+//     y: 200,
+//     duration: 2 
+// })
+
+//Yo defino donde empieza y donde terimna la animacion con fromTo
+gsap.fromTo('.box', {
+    //En los objetos, siempre se pone el estado inicial, y luego el estado final, y se pueden poner las propiedades que se quieran en cada uno de los objetos, como la duracion, o la facilidad de la animacion
+    y: 0,
+    //Este duration es ignorado por GSAP
+    duration: 2,
+},
+{
+    //este segundo objeto es el que manda
+     y: 600,
+     //Este duration es el que manda, el primero es ignorado por GSAP
+    duration: 2,
+    background: "#f93",
+    repeat:-1,
+    yoyo: true,
+    ease: "power1.inOut",
+    delay: 1
+})
+
+
+
+
+
+
+
+
+
+
+
+// //el default ayuda a no tener que escribir la duracion en cada animacion, se puede sobreescribir en cada animacion si se quiere
+
+// const tl = gsap.timeline({ defaults: { duration: 3 } })
+//     tl.fromTo('.word', 
+//   { x: "+=100", opacity: 10 }, // Inicio
+//   { x: 0, opacity: 1 }    // Fin (su posición natural en el CSS es x: 0)
+// )
+// //from define desde su estado inicial que es 180px a su posición natural que es x:0
+//     tl.from('.wordTwo', { x: 180,  opacity: 0 }, {opacity: 1})
+// //     El secreto: x: "+=100"
+// // GSAP tiene una sintaxis especial para valores relativos que es extremadamente potente:
+// // x: 100: Mueve el elemento a la coordenada absoluta 100px.
+// // x: "+=100": Toma la posición donde esté el elemento ahora mismo y le suma 100px.
+//     tl.to('.wordThree', { x: "+=100", opacity: 1 , duration: 1.5, ease: "power1.inOut" } , "<")
+//    // x: "-=100": Toma la posición donde esté el elemento ahora mismo y le resta 100px.
+//     // tl.fromTo('.wordThree', { x: 500, opacity: 1 , duration: 1.5, ease: "power1.inOut" , x: "-=100" } , "<")
+
+// GSDevTools.create({ animation: tl })
+
+//Escribe el texto que existe aca
+//TextPlugin
+// gsap.to('p', {
+//     text: "Luis Linares Design Engineering",
+//     repeat: 1,
+//     duration: 4,
+//     ease: "power1.in",
+// })
+
+//maquina de escribir reemplazo efecto
+//Con text
+// gsap.from('p', {
+//      text: "Im learning a lot of things about animation and I am loving it",
+//      repeat: 1,
+//      yoy:true,
+//      duration: 4,
+//   ease: "power1.in",
+// })
+
+gsap.to('p', {
+    duration: 2,
+    text: {
+        value: "Analizando datos del sistema..",
+        // " " asi lo hace palabra por palabra, si se pone "" lo hace letra por letra
+        delimiter: " ",
+        duration: 0.5,
+    },
+    ease: "none",
+})
+
+// gsap.to('.status', {
+//     text: {
+//         value: "Error: No se han encontrado datos",
+//         newClass: "highlight",
+//     },
+//     duration: 0.5,
+// })
+
+//cambia el texto de un elemento a otro, con el tipo diff hace que solo cambie lo que es diferente entre el texto inicial y el final, haciendo una animacion mas fluida
+gsap.to('.title', {
+    text: {
+        value: "Estamos en un mundo donde se aprenden pocas cosas",
+        type: "diff",
+    },
+    duration: 0.5
+})
+
+
+// rtl (Right-to-Left)
+// Muy útil si trabajas para mercados internacionales (árabe, hebreo) o si simplemente buscas un efecto creativo donde el texto parece "emerger" desde el final hacia el principio.
+gsap.to(".footer-text", {
+  text: {
+    value: "¡Gracias por leer!",
+    rtl: true
+  },
+  duration: 2
+});
+
+gsap.registerPlugin(TextPlugin);
+
--- src/main.js
+++ src/main.js
@@ -2,83 +2,122 @@ import './style.css'
 import gsap from 'gsap'
 
 
-const charT = document.querySelector('.charH')
-const charO = document.querySelector('.charO')
-const charM = document.querySelector('.charM')
-const charE = document.querySelector('.charE')
-const dot = document.querySelector('.dot')
+// const charT = document.querySelector('.charH')
+// const charO = document.querySelector('.charO')
+// const charM = document.querySelector('.charM')
+// const charE = document.querySelector('.charE')
+// const dot = document.querySelector('.dot')
+
+// const tweentT =  gsap.to('.charH', {
+//     color: 'white ',
+//     x: 10,
+//     paused: true
+// })
+
+// const tweentO =  gsap.to('.charO', {
+//     color: 'white',
+//         x: 10,
+//     paused: true
+// })
+
+// const tweentM =  gsap.to('.charM', {
+// color: 'white', 
+//         x: 10,
+//     paused: true
+// })
+
+// const tweentE =  gsap.to('.charE', {
+//     color: 'white',
+//         x: 10,
+//     paused: true
+// })
+
+// dot.addEventListener("mouseenter", () => {
+//     dotTween.play()
+// });
+
+// dot.addEventListener("mouseleave", () => {
+//     dotTween.reversed()
+// });
 
 
-const dotTween = gsap.to('.dot', {
-    backgroundColor: '#F93',
-    x: 5,
-    scale: 1.5,
-    paused: true
-})
+// charT.addEventListener("mouseenter", () => {
+//     tweentT.play()
+    
+// });
+// charT.addEventListener("mouseleave", () => {
+//     tweentT.paused()
+// });
 
-const tweentT =  gsap.to('.charH', {
-    color: 'white ',
-    x: 10,
-    paused: true
-})
+// charO.addEventListener("mouseenter", () => {
+//     tweentO.play()
+    
+// });
+// charO.addEventListener("mouseleave", () => {
+//     tweentO.paused()
+// });
 
-const tweentO =  gsap.to('.charO', {
-    color: 'white',
+// charM.addEventListener("mouseenter", () => {
+//     tweentM.play()
+    
+// });
+// charM.addEventListener("mouseleave", () => {
+//     tweentM.paused()
+// });
+
+// charE.addEventListener("mouseenter", () => {
+//     tweentE.play()
+    
+// });
+// charE.addEventListener("mouseleave", () => {
+//     tweentE.paused()
+// });
+
+// Enfoque B: Hover individual por cada letra
+// Si tu objetivo original era que solo la letra que tocas con el mouse reaccione (independientemente de las demás), entonces el loop forEach no debe ir en el contenedor .home, sino directamente en las letras.
+const home = document.querySelectorAll('.charH, .charO, .charM, .charE')
+
+home.forEach(function(homeItem){
+    const letterTeen = gsap.to(homeItem, {
+  color: 'white',
         x: 10,
-    paused: true
+        duration: 0.2,
+        paused: true,
+        ease: 'power1.out',
+        yoyo: true,
+
+    })
+
+    homeItem.addEventListener('mouseenter', () => {
+        letterTeen.play()
+    })
+
+    homeItem.addEventListener('mouseleave', () => {
+        letterTeen.reverse()
+        // hacer que la letra vuelva a su estado original, en este caso el color y la posición, pero con una animación suave. La función reverse() hace exactamente eso: reproduce la animación en sentido inverso, devolviendo la letra a su estado inicial de manera fluida.
+        gsap.to(homeItem, {color: 'black', x: 0, duration: 0.2, ease: 'power1.out'})
+    })
 })
 
-const tweentM =  gsap.to('.charM', {
-color: 'white', 
-        x: 10,
-    paused: true
+
+
+// Enfoque A: Hover en el contenedor .home para que todas las letras reaccionen al mismo tiempo
+const items = document.querySelectorAll('.item')
+    //gsap.defaults means that all the tweens that we create will have this duration by default, so we don't have to specify it in each tween
+    gsap.defaults({duration: 0.3})
+items.forEach(function(item, index)  {
+    console.log(item, index)
+    const tween = gsap.timeline({paused: true})
+    .to(item.querySelector('.text'), {color: 'white', x: 10, scale: 1.3 , transformOrigin: 'left center'})
+    .to(item.querySelector('.dot'), {backgroundColor: '#F93', x: 5, scale: 1.5}, '<') // el '<' significa que esta animacion se va a ejecutar al mismo tiempo que la anterior
+ 
+    item.addEventListener('mouseenter', () => {
+        tween.play()
+    })
+
+    item.addEventListener('mouseleave', () => {
+        tween.reverse()
+    })
+
 })
-
-const tweentE =  gsap.to('.charE', {
-    color: 'white',
-        x: 10,
-    paused: true
-})
-
-dot.addEventListener("mouseenter", () => {
-    dotTween.play()
-});
-
-dot.addEventListener("mouseleave", () => {
-    dotTween.paused()
-});
-
-
-charT.addEventListener("mouseenter", () => {
-    tweentT.play()
-    
-});
-charT.addEventListener("mouseleave", () => {
-    tweentT.paused()
-});
-
-charO.addEventListener("mouseenter", () => {
-    tweentO.play()
-    
-});
-charO.addEventListener("mouseleave", () => {
-    tweentO.paused()
-});
-
-charM.addEventListener("mouseenter", () => {
-    tweentM.play()
-    
-});
-charM.addEventListener("mouseleave", () => {
-    tweentM.paused()
-});
-
-charE.addEventListener("mouseenter", () => {
-    tweentE.play()
-    
-});
-charE.addEventListener("mouseleave", () => {
-    tweentE.paused()
-});
-
 
